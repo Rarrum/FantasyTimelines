585bd80d470f79ec1eb04bc73642681179bdca3a
luke <rarrum@gmail.com>
2025-02-09 14:04:53 -0800

    Initial world and timeline placeholders.

---
+++ src/WorldState/WorldState.ixx
@@ -0,0 +1,7 @@
+module;
+
+export module WorldState;
+
+export import :Timeline;
+export import :World;
+export import :Character;
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,19 @@
 #include <QApplication>
 #include <QWidget>
 
+import WorldState;
+
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
     QWidget window = QWidget();
     window.resize(800, 600);
     window.show();
+
+    //temp to verify module stuff is working
+    Timeline timeline;
+    WorldState world;
+    Character character;
 
     return app.exec();
 }
